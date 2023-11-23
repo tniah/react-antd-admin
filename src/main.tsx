cb77cd028c14101d86dcf47ccac6e53faf67818c
Makai <tronghaibk2008@gmail.com>
2023-11-23 13:26:32 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
@@ -7,11 +6,9 @@ import store from './stores';
 import './styles/index.less';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <Provider store={ store }>
-      <App/>,
-    </Provider>
-  </React.StrictMode>
+ReactDOM.render(
+  <Provider store={ store }>
+    <App/>
+  </Provider>,
+  document.getElementById('root'),
 );
